53b165a4319d75d4ea76983f7324093ce6151220
Alexandr Vezhlev <avezhlev@gmail.com>
2019-06-25 20:43:49 +0500

    General improvements
    - Added holder for mapping result to properly handle stream exceptions
    - Implemented fallback to flat-mapping pipeline if the files number exceeds the specified value (to conform OS limit on open files per process)
    - Added documentation

--- src/main/java/com/task/processor/LogFilesDirProcessor.java
+++ src/main/java/com/task/processor/LogFilesDirProcessor.java
@@ -1,14 +1,18 @@
 package com.task.processor;
 
 import com.task.util.StreamUtils;
+import com.task.util.StreamUtils.MappingResult;
 import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -17,27 +21,30 @@ import java.util.stream.Stream;
 @RequiredArgsConstructor
 public class LogFilesDirProcessor<T> {
 
+    // This value should correlate with and be less than OS limit on open file descriptors per process.
+    // Be warned that the algorithm uses common ForkJoinPool,
+    // so make sure that at least (ForkJoinPool.getCommonPoolParallelism() + 1) file descriptors are allowed by OS.
+    private final static int MAX_ALLOWED_OPEN_FILE_DESCRIPTORS = Math.max(512, ForkJoinPool.getCommonPoolParallelism() + 1);
+
     private final Function<String, ? extends T> fromLineMapper;
     private final Function<? super T, String> toLineMapper;
 
-    public long process(String logFilesDirPath, String resultFilePath, Predicate<T> filter, Comparator<T> comparator) throws IOException {
-        AtomicLong counter = new AtomicLong(0);
+    public long process(String inputDirPath, String outputFilePath, Predicate<T> filter, Comparator<T> comparator) throws IOException {
+        LongAdder counter = new LongAdder();
         try (Stream<String> pipeline =
-                     configurePipeline(Paths.get(logFilesDirPath), filter, comparator)
-                             .peek(item -> counter.getAndIncrement())) {
-            Files.write(Paths.get(resultFilePath), (Iterable<String>) pipeline::iterator);
-            return counter.get();
+                     configurePipeline(Paths.get(inputDirPath), filter, comparator)
+                             .peek(item -> counter.increment())) {
+            Files.write(Paths.get(outputFilePath), (Iterable<String>) pipeline::iterator);
+            return counter.sum();
         }
     }
 
-    private Stream<String> configurePipeline(Path logFilesDirPath, Predicate<T> filter, Comparator<T> comparator) throws IOException {
-        try (Stream<Path> filesStream = Files.list(logFilesDirPath)) {
-            return filesStream
+    private Stream<String> configurePipeline(Path inputDirPath, Predicate<T> filter, Comparator<T> comparator) throws IOException {
+        try (Stream<Path> filesStream = Files.list(inputDirPath)) {
+            List<Path> files = filesStream
                     .filter(Files::isRegularFile)
-                    .map(LogFilesDirProcessor::getFileLinesStreamUnchecked)
-                    .collect(Collectors.collectingAndThen(
-                            Collectors.toList(),
-                            StreamUtils::balancingConcat))
+                    .collect(Collectors.toList());
+            return upstreamPipeline(files)
                     .map(fromLineMapper)
                     .filter(Objects::nonNull)
                     .filter(filter)
@@ -47,11 +54,59 @@ public class LogFilesDirProcessor<T> {
         }
     }
 
-    private static Stream<String> getFileLinesStreamUnchecked(Path path) {
-        try {
-            return Files.lines(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private Stream<String> upstreamPipeline(List<Path> files) {
+        return files.size() > MAX_ALLOWED_OPEN_FILE_DESCRIPTORS ?
+                flatMappedPipeline(files) :
+                concatenatedPipeline(files);
+    }
+
+    /**
+     * Provides better parallelism, especially if used with JDK 9+
+     * (allowing to process groups of lines from the same file by different threads),
+     * but implies trade-off in a form of larger number of simultaneously open file descriptors.
+     *
+     * @param files A list of file paths.
+     * @return A stream of lines from the specified files.
+     */
+    private Stream<String> concatenatedPipeline(List<Path> files) {
+        return initialPipeline(files)
+                .map(MappingResult::getMappedValue)
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toList(),
+                        StreamUtils::balancingConcat));
+    }
+
+    /**
+     * Provides worse parallelism (a file is fully processed by one thread due to flat-mapping),
+     * but also constrains the number of simultaneously open file descriptors to the number of processing threads.
+     *
+     * @param files A list of file paths.
+     * @return A stream of lines from the specified files.
+     */
+    private Stream<String> flatMappedPipeline(List<Path> files) {
+        return initialPipeline(files)
+                .flatMap(MappingResult::getMappedValue);
+    }
+
+    /**
+     * Common initial pipeline that wraps mapping results to properly handle and log exceptions.
+     *
+     * @param files A list of file paths. The {@link List} interface is used on purpose,
+     *              since spliterators provided by JDK list implementations provide good task splitting capabilities.
+     * @return A stream of successful attempts to map a file to its lines stream.
+     */
+    private Stream<MappingResult<Path, Stream<String>>> initialPipeline(List<Path> files) {
+        return files
+                .stream()
+                .map(MappingResult.wrap(Files::lines))
+                .peek(this::logStreamOpeningResultIfFailed)
+                .filter(MappingResult::isSuccessful);
+    }
+
+    private void logStreamOpeningResultIfFailed(MappingResult<Path, Stream<String>> result) {
+        if (!result.isSuccessful()) {
+            System.out.println("Skipping failed to open file " +
+                    "'" + result.getInitialValue().toAbsolutePath() + "': " + result.getMappingException());
         }
     }
 
--- src/main/java/com/task/util/StreamUtils.java
+++ src/main/java/com/task/util/StreamUtils.java
@@ -1,10 +1,22 @@
 package com.task.util;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class StreamUtils {
 
+    /**
+     * Provides a balanced tree of concatenated streams.
+     *
+     * @param streams A list of streams to concat.
+     * @param <T>     The type of the stream elements.
+     * @return A concatenated stream.
+     */
     public static <T> Stream<T> balancingConcat(List<Stream<T>> streams) {
         return doBalancingConcat(streams, 0, streams.size());
     }
@@ -22,4 +34,48 @@ public class StreamUtils {
                 return Stream.concat(left, right);
         }
     }
+
+    /**
+     * Provides a mapping result for usage in streams. Allows to properly handle exceptions.
+     *
+     * @param <T> Value type before mapping.
+     * @param <R> Value type after mapping.
+     */
+    @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class MappingResult<T, R> {
+
+        private final T initialValue;
+        private final Exception mappingException;
+        private final R mappedValue;
+
+        private static <T, R> MappingResult<T, R> success(R value) {
+            return new MappingResult<>(null, null, value);
+        }
+
+        private static <T, R> MappingResult<T, R> failure(T initialValue, Exception exception) {
+            return new MappingResult<>(initialValue, exception, null);
+        }
+
+        public static <T, R> Function<T, MappingResult<T, R>> wrap(CheckedFunction<T, R> function) {
+            return t -> {
+                try {
+                    return success(function.apply(t));
+                } catch (Exception e) {
+                    return failure(t, e);
+                }
+            };
+        }
+
+        public boolean isSuccessful() {
+            return mappedValue != null;
+        }
+
+        @FunctionalInterface
+        public interface CheckedFunction<T, R> {
+            R apply(T t) throws Exception;
+        }
+
+    }
+
 }
